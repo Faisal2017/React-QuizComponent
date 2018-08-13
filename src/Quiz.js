435aa8c71ada88276d2dcdeea472f9d73c4ef341
Faisal Ali <faisal.ali@woodmac.com>
2018-08-13 14:57:57 +0100

    module 5

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,7 +1,30 @@
+// eslint-disable-next-line
 import React, {Component} from 'react';
 import QuizQuestion from './QuizQuestion';
+import QuizEnd from './QuizEnd';
+
+import { Button } from 'semantic-ui-react';
+import { Menu } from 'semantic-ui-react';
 
 let quizData = require('./quiz_data.json');
+
+class ButtonExampleButton extends React.Component {
+    render() {
+        return (
+            <div> 
+                <Button>Click Here</Button>
+            </div>
+        )
+    }
+}
+
+
+const MenuExampleHover = () => (
+  <Menu compact>
+    <Menu.Item as='a'>Link 1</Menu.Item>
+    <Menu.Item link>Link 2</Menu.Item>
+  </Menu>
+)
 
 class Quiz extends React.Component {
     constructor(props) {
@@ -10,9 +33,15 @@ class Quiz extends React.Component {
     }
 
     render() {
+
+        const isQuizEnd = (this.state.quiz_position - 1) - quizData.quiz_questions.length;
+
         return (
             <div>
-                <QuizQuestion quiz_questions={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+                <MenuExampleHover />
+                <ButtonExampleButton />
+                <QuizEnd />
+                <QuizQuestion quiz_questions={quizData.quiz_questions[this.state.quiz_position - 1]} />
             </div>
         );
     }
